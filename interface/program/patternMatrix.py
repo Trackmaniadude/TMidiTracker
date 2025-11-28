f807e0e3ca82b98317f4e90e3dbbd765eb4a6b19
Trackmaniadude <alex108@tringali.org>
2025-11-28 17:59:27 -0500

    add scrollframe to pattern matrix

--- interface/program/patternMatrix.py
+++ interface/program/patternMatrix.py
@@ -9,6 +9,8 @@ if TYPE_CHECKING:
     from structures.channel import Channel
     from structures.pattern import Pattern
 
+from interface.utilities.doubleScrollFrame import DScrollFrame
+
 import logging
 import tkinter as tk
 from tkinter import ttk
@@ -19,3 +21,8 @@ _logger = logging.getLogger(__name__)
 class PatternMatrix(ttk.Frame):
     def __init__(self, parent: tk.Misc):
         super().__init__(parent, relief="raised", width=200, height=200)
+
+        sf = DScrollFrame(self, mode="DOUBLE")
+        sf.pack(fill="both", expand=True)
+
+        self.__content = sf.content
