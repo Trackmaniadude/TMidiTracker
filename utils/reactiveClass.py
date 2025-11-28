d590ae0a8d91ad1c6db617a0999a7a4b010df9f6
Trackmaniadude <alex108@tringali.org>
2025-11-28 17:43:01 -0500

    reactive class can track changes to dict and list attributes

--- utils/reactiveClass.py
+++ utils/reactiveClass.py
@@ -2,27 +2,102 @@
 Gives a class a Changed event, fired when any value is changed.
 """
 
+import logging
 from typing import Any
 
 from utils.event import Event
 
+_logger = logging.getLogger(__name__)
+
+
+class ReactiveContainer[TKey, TContent]:
+    def __init__(self) -> None:
+        self.Changed: Event[[TKey, TContent, TContent]] = Event()
+        """Event(index: int, old: T, new: T)"""
+
+
+class ReactiveList[TContent](ReactiveContainer):
+    def __init__(self, list: list[TContent]) -> None:
+        super().__init__()
+        self.__container = list
+
+    def __getitem__(self, index: int) -> TContent:
+        return self.__container[index]
+
+    def __setitem__(self, index: int, value: TContent):
+        old = self.__container[index] if index in self.__container else None
+        self.__container[index] = value
+        if old != value:
+            self.Changed.fire(index, old, value)
+
+    def __contains__(self, v):
+        return v in self.__container
+
+
+class ReactiveDict[TKey, TContent](ReactiveContainer):
+    def __init__(self, dict: dict[TKey, TContent]) -> None:
+        super().__init__()
+        self.__container = dict
+        self.get = dict.get
+
+    def __getitem__(self, key: TKey) -> TContent:
+        return self.__container[key]
+
+    def __setitem__(self, key: TKey, value: TContent):
+        old = self.__container[key] if key in self.__container else None
+        self.__container[key] = value
+        if old != value:
+            self.Changed.fire(key, old, value)
+
+    def __contains__(self, v):
+        return v in self.__container
+
+
+attribute = object()
+
 
 class ReactiveClass:
     def __init__(self) -> None:
-        self.Changed: Event[[str, Any, Any]] = Event()
-        """Fired when any attribute is changed. (name: str, old: Any, new: Any)"""
+        self.Changed: Event[[str, Any, Any, Any]] = Event()
+        """
+        Fired when any attribute is changed. (name: str, key: Any, old: Any, new: Any)
+        Key is set if a container was changed. Otherwise it is the attribute singleton.
+        """
 
-    def getPropertyChangedEvent(self, name: str) -> Event[[Any, Any]]:
+        if _logger.getEffectiveLevel() >= logging.DEBUG:
+            self.Changed.connect(lambda *_: _logger.debug(_))
+
+    def setupContainerListen(self):
+        for name in dir(self):
+            if name.startswith("__"):
+                continue
+            obj = getattr(self, name)
+            if type(obj) is list:
+                r = ReactiveList(obj)
+            elif type(obj) is dict:
+                r = ReactiveDict(obj)
+            else:
+                continue
+
+            def q(name):  # Wonky to make it remember name
+                r.Changed.connect(
+                    lambda key, old, new: self.Changed.fire(name, key, old, new)
+                )
+
+            q(name)
+            super().__setattr__(name, r)
+
+    def getPropertyChangedEvent(self, name: str) -> Event[[Any, Any, Any]]:
         """
         Get an event that fires when a specific property is changed.
-        Event args: (old: Any, new: Any)
+        Event args: (key: Any, old: Any, new: Any)
         """
         # TODO: Is there a way to type the event?
         e = Event()
 
-        def h(changedName: str, old: Any, new: Any):
+        def h(changedName: str, key: Any, old: Any, new: Any):
             if name == changedName:
-                e.fire(old, new)
+                e.fire(key, old, new)
 
         self.Changed.connect(h)
         return e
@@ -33,6 +108,6 @@ class ReactiveClass:
             old = self.__getattribute__(name)
             super().__setattr__(name, value)
             if old != value:
-                self.Changed.fire(name, old, value)
+                self.Changed.fire(name, attribute, old, value)
         else:
             super().__setattr__(name, value)
