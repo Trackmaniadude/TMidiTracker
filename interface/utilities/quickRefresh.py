2487779fa066a115d3db1e02990683eb89a6b47e
Trackmaniadude <alex108@tringali.org>
2026-02-11 12:42:11 -0500

    quickrefresh mixin (instead of directly refreshing, queues a refresh in tk (if a flag is not already set, which is unset when by the refresh))

---
+++ interface/utilities/quickRefresh.py
@@ -0,0 +1,29 @@
+import random
+from abc import ABC, abstractmethod
+
+
+class QuickRefresh(ABC):
+    """
+    Adds a "queueRefresh" method, which will tell tk to call the object's "refresh" method when next possible.
+    Uses a flag to avoid queuing multiple refreshes in one frame.
+    """
+
+    # def __init__(self) -> None:
+    #     self._refreshQueueFlag: bool = False
+
+    def queueRefresh(self):
+        try:
+            if not self._refreshQueueFlag:
+                self._refreshQueueFlag = True
+                print(f"TEST {random.randint(1, 100)}")
+                self.after_idle(self.refresh)  # type: ignore
+        except AttributeError:
+            # TODO: figure out how to properly initialize the data (why isnt it working?)
+            self._refreshQueueFlag = False
+
+    def resetRefreshFlag(self):
+        # TODO: is there a way to make this automatic?
+        self._refreshQueueFlag = False
+
+    @abstractmethod
+    def refresh(self): ...
