d897ec7cf285c3c064e5b36ce0217b68162ce269
Trackmaniadude <alex108@tringali.org>
2026-02-13 12:42:42 -0500

    show instrument numbers in list overview

--- interface/program/instrumentList.py
+++ interface/program/instrumentList.py
@@ -6,10 +6,18 @@ from interface.utilities.headerFrame import HeaderFrame
 from utils.gm import GMDrumKits, GMEntry, GMPrograms
 
 
+def toHex2D(n: int) -> str:
+    s = hex(n)[2:].upper()
+    if n < 16:
+        return "0" + s
+    else:
+        return s
+
+
 class InstrumentList(ttk.Frame):
     def __init__(self, parent: tk.Misc):
         super().__init__(parent)
-        self.config(relief="raised", width=220, height=20, borderwidth=2)
+        self.config(relief="raised", width=280, height=20, borderwidth=2)
 
         sf = DScrollFrame(self, mode="VERTICAL")
         sf.pack(fill="both", expand=True)
@@ -32,12 +40,18 @@ class InstrumentList(ttk.Frame):
         process(GMDrumKits)
 
         for category, data in categories.items():
-            hf = HeaderFrame(sf.content, category, userCollapsible=True)
+            mn = min([t[0] for t in data]) - 1
+            mx = max([t[0] for t in data]) - 1
+            hf = HeaderFrame(
+                sf.content,
+                f"{category} {toHex2D(mn)} - {toHex2D(mx)}",
+                userCollapsible=True,
+            )
             hf.collapse()
             hf.pack(side="top", fill="x")
             for entry in data:
                 value = entry[0]
                 name = entry[1]
-                ttk.Label(hf.content, text=f"{hex(value)[2:].upper()}: {name}").pack(
+                ttk.Label(hf.content, text=f"{toHex2D(value - 1)}: {name}").pack(
                     side="top", fill="x"
                 )
