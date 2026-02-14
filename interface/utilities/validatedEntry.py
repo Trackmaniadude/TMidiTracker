76bd6e38a6a99976cc257f9e81a29f31c50b085e
Trackmaniadude <alex108@tringali.org>
2026-02-14 13:42:11 -0500

    bring in validatedentry from another project

---
+++ interface/utilities/validatedEntry.py
@@ -0,0 +1,124 @@
+import tkinter as tk
+from abc import ABC, abstractmethod
+from tkinter import ttk
+from typing import Protocol
+
+from utils.event import Event
+
+ValidateableEntry = tk.Entry | tk.Spinbox | ttk.Entry | ttk.Spinbox | ttk.Combobox
+
+
+class ValidatorFunc(Protocol):
+    """Signature for validation functions."""
+
+    @staticmethod
+    def __func__(s: str) -> str | None: ...
+
+
+class AbstractValidator(ABC):
+    """Abstract class for validators."""
+
+    @abstractmethod
+    def validate(self, s: str) -> str | None: ...
+
+
+class Validators:
+    """Collection of validators."""
+
+    class Through(AbstractValidator):
+        """Does nothing."""
+
+        def validate(self, s: str) -> str | None:
+            return s
+
+    class Numeric(AbstractValidator):
+        """Requires the entry to be numerical, as well as limits on the number."""
+
+        def __init__(
+            self,
+            min: float | None = None,
+            max: float | None = None,
+            *,
+            round: float | None = None,
+        ) -> None:
+            self.min = min
+            self.max = max
+            self.round = round
+
+        def validate(self, s: str) -> str | None:
+            try:
+                v = float(s)
+                if self.round:
+                    v = round(v / self.round) * self.round
+                if self.min is not None:
+                    v = max(v, self.min)
+                if self.max is not None:
+                    v = min(v, self.max)
+                return str(v)
+            except:
+                return None
+
+    class List(AbstractValidator):
+        """Requires the entry to be a member of a list."""
+
+        def __init__(self, values: list[str]) -> None:
+            self.values = set(values)
+
+        def validate(self, s: str) -> str | None:
+            if s in self.values:
+                return s
+            return None
+
+
+class ValidatedEntry[Entry: ValidateableEntry]:
+    """Entry wrapper which adds validation logic, as well as some other niceties."""
+
+    def __init__(self, entry: Entry, validator: AbstractValidator) -> None:
+        self.__var = tk.StringVar(entry, entry.get())
+        self.__validator = validator
+        self.__value = self.__var.get()
+
+        def _v(*_):
+            val = self.__validator.validate(self.__var.get())
+            if val:  # Success
+                if val != self.__value:  # Don't fire if no actual change happened
+                    self.__value = val
+                    self.__var.set(val)
+                    self.Changed.fire(True)
+            else:  # Failure
+                self.__var.set(self.__value)
+                self.Error.fire(True)
+
+        entry.bind("<Return>", _v)
+        entry.bind("<FocusOut>", _v)
+        entry.bind("<<ComboboxSelected>>", _v)
+        entry.bind("<<Increment>>", _v)
+        entry.bind("<<Decrement>>", _v)
+        entry.config(textvariable=self.__var)
+        self.entry = entry
+
+        self.Changed: Event[[bool]] = Event()
+        """Fired on a successful change to the variable. Provides one bool, if the event was caused by user input."""
+        self.Error: Event[[bool]] = Event()
+        """Fired on an unsuccessful change to the variable. Provides one bool, if the event was caused by user input."""
+
+    def get(self) -> str:
+        return self.__value
+
+    def set(self, s: str, *, ignoreValidation: bool = False):
+        val = self.__validator.validate(s) if not ignoreValidation else s
+        if val:  # Success
+            if val != self.__value:  # Don't fire if no actual change happened
+                self.__value = val
+                self.__var.set(val)
+                self.Changed.fire(False)
+        else:  # Failure
+            self.Error.fire(False)
+
+    @property
+    def value(self) -> str:
+        return self.get()
+
+    @value.setter
+    def value(self, s: str):
+        self.set(s)
---
+++ interface/utilities/validatedEntryPrebuilts.py
@@ -0,0 +1,121 @@
+import tkinter as tk
+from abc import ABC, abstractmethod
+from enum import Enum
+from tkinter import ttk
+
+from interface.utilities.validatedEntry import ValidatedEntry, Validators
+from utils.event import Event
+
+_BIG_NUM = (
+    2**63
+)  # Arbitrary, does python have a 'Number that is greater than all others' like Lua's math.huge?
+
+
+class AbstractPrebuilt[T](ABC):
+    """Abstract class for prebuilt entries."""
+
+    @property
+    @abstractmethod
+    def value(self) -> T: ...
+    @value.setter
+    @abstractmethod
+    def value(self, val: T): ...
+
+    Changed: Event[bool]
+    Error: Event[bool]
+
+
+class Prebuilts:
+    """Prebuilt specialized entries."""
+
+    class Spinbox(AbstractPrebuilt):
+        """Number input with min/max and rounding."""
+
+        def __init__(
+            self,
+            parent: tk.Misc,
+            *,
+            default: float | None = None,
+            range: tuple[float, float] | None = None,
+            increment: float = 1,
+            round: float | None = None,
+        ):
+            min = range[0] if range is not None else None
+            max = range[1] if range is not None else None
+
+            if default is None:
+                if min is not None:
+                    default = min
+                elif max is not None:
+                    default = max
+                else:
+                    default = 0
+
+            self.entry = ttk.Spinbox(
+                parent, from_=min or -_BIG_NUM, to=max or _BIG_NUM, increment=increment
+            )
+            self.entry.set(default)
+
+            self.vEntry = ValidatedEntry(
+                self.entry, Validators.Numeric(min, max, round=round)
+            )
+
+            self.Changed = self.vEntry.Changed
+            self.Error = self.vEntry.Error
+
+        @property
+        def value(self) -> float:
+            return float(self.vEntry.value)
+
+        @value.setter
+        def value(self, val: float):
+            self.vEntry.value = str(val)
+
+    class Enum[T: Enum](AbstractPrebuilt):
+        """Select from an enum."""
+
+        def __init__(self, parent: tk.Misc, enum: type[T], *, default: T | None = None):
+            values = [item.name for item in enum]
+            self.enum = enum
+
+            self.entry = ttk.Combobox(parent, values=values)
+            self.entry.config(state="readonly")
+            self.entry.set(default or values[0])
+
+            self.vEntry = ValidatedEntry(self.entry, Validators.List(values))
+
+            self.Changed = self.vEntry.Changed
+            self.Error = self.vEntry.Error
+
+        @property
+        def value(self) -> T:
+            return self.enum[self.vEntry.value]
+
+        @value.setter
+        def value(self, val: T):
+            self.vEntry.value = val.name
+
+    class List(AbstractPrebuilt):
+        """Select from a list."""
+
+        def __init__(
+            self, parent: tk.Misc, items: list[str], *, default: str | None = None
+        ):
+            self.items = items
+
+            self.entry = ttk.Combobox(parent, values=items)
+            self.entry.config(state="readonly")
+            self.entry.set(default or items[0])
+
+            self.vEntry = ValidatedEntry(self.entry, Validators.List(items))
+
+            self.Changed = self.vEntry.Changed
+            self.Error = self.vEntry.Error
+
+        @property
+        def value(self) -> str:
+            return self.vEntry.value
+
+        @value.setter
+        def value(self, val: str):
+            self.vEntry.value = val
