8c77dd237b7d39f382a76d0247cc51ba1bf717c2
Trackmaniadude <alex108@tringali.org>
2026-02-19 23:26:31 -0500

    songdataview using dictsettingeditor

--- interface/program/songDataView.py
+++ interface/program/songDataView.py
@@ -3,43 +3,65 @@
 import tkinter as tk
 from tkinter import ttk
 
+from interface.utilities.dictSettingsEditor import DictSettingsEditor
+from interface.utilities.dictSettingsEditorEntries import DSEEntries
 from interface.utilities.doubleScrollFrame import DScrollFrame
 from interface.utilities.headerFrame import HeaderFrame
 from interface.utilities.validatedEntryPrebuilts import Prebuilts
+from structures import program
 
 
 class SongDataView(ttk.Frame):
     def __init__(self, parent: tk.Misc):
-        super().__init__(parent, relief="raised", width=300, height=200, borderwidth=2)
+        super().__init__(parent, relief="raised", width=300, height=0, borderwidth=2)
 
-        sf = DScrollFrame(self, mode="VERTICAL", propagationMode="contentDrivesFrame")
+        self.pack_propagate(False)
+        sf = DScrollFrame(self, mode="VERTICAL", propagationMode="frameDrivesContent")
         sf.pack(fill="both", expand=True)
         self.content = sf.content
-        # self.pack_propagate(False)
 
-        metadataFrame = HeaderFrame(self.content, "Metadata", userCollapsible=True)
-        metadataFrame.pack(side="top", fill="x")
+        metadataEdit = DictSettingsEditor(
+            self.content,
+            program.p.currentSong.metadata.__dict__,
+            "Metadata",
+            autoApply=True,
+            collapsible=True,
+        )
+        metadataEdit.pack(side="top", fill="x", expand=False)
+        metadataEdit.addValueEdit("title", DSEEntries.SmallTextbox(), "Title")
+        metadataEdit.addValueEdit("author", DSEEntries.SmallTextbox(), "Author")
+        metadataEdit.addValueEdit("genre", DSEEntries.SmallTextbox(), "Genre")
+        metadataEdit.addValueEdit("notes", DSEEntries.LargeTextbox(), "Notes")
 
-        # TODO: nice modular way to do this
+        timeEdit = DictSettingsEditor(
+            self.content,
+            program.p.currentSong.__dict__,
+            "Time/Speed",
+            autoApply=True,
+            collapsible=True,
+        )
+        timeEdit.pack(side="top", fill="x", expand=False)
+        timeEdit.addValueEdit("clock", DSEEntries.Integer(min=1, max=1000), "Clock")
 
-        ttk.Label(metadataFrame.content, text="Title").grid(
-            row=0, column=0, sticky="we"
+        structureEdit = DictSettingsEditor(
+            self.content,
+            program.p.currentSong.__dict__,
+            "Song Structure",
+            autoApply=True,
+            collapsible=True,
         )
-        ttk.Entry(metadataFrame.content).grid(row=0, column=1, sticky="e")
-        ttk.Label(metadataFrame.content, text="Author").grid(
-            row=1, column=0, sticky="we"
+        structureEdit.pack(side="top", fill="x", expand=False)
+        structureEdit.addValueEdit(
+            "visibleChannels", DSEEntries.Integer(min=0, max=14), "Channels"
         )
-        ttk.Entry(metadataFrame.content).grid(row=1, column=1, sticky="e")
-        ttk.Label(metadataFrame.content, text="Genre").grid(
-            row=2, column=0, sticky="we"
+        structureEdit.addValueEdit(
+            "patternLength", DSEEntries.Integer(min=1, max=255), "Pattern Size"
         )
-        ttk.Entry(metadataFrame.content).grid(row=2, column=1, sticky="e")
-        ttk.Label(metadataFrame.content, text="Genre").grid(
-            row=3, column=0, sticky="we", columnspan=2
+        structureEdit.addValueEdit(
+            "majorSubdiv", DSEEntries.Integer(min=1, max=10000), "Major Subdivision"
         )
-        t = tk.Text(metadataFrame.content, width=10, height=10)
-        t.grid(row=4, column=0, sticky="we", columnspan=2)
-        t.bind("<space>", lambda *_: _, "")
-
-        metadataFrame.content.columnconfigure(0, weight=1)
-        metadataFrame.collapse()
+        structureEdit.addValueEdit(
+            "minorSubdiv",
+            DSEEntries.Integer(min=1, max=10000),
+            "Minor Subdivision",
+        )
